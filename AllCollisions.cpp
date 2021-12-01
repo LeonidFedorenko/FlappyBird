bccf039b65138f836608128b1136b818e21b8802
Leonid Fedorenko <47683018+LeonidFedorenko@users.noreply.github.com>
2021-12-01 22:05:46 +1000

    Update AllCollisions.cpp

--- AllCollisions.cpp
+++ AllCollisions.cpp
@@ -1,18 +1,18 @@
-// äëÿ ñòîëêíîâåíèé â èãðå 
+// для столкновений в игре
 #include "AllCollisions.hpp"
 
 namespace SoundSonar
-{	// êîíñòðóêòîð ñòîëêíîâåíèé
+{	// конструктор столкновений
 	AllCollisions::AllCollisions()
 	{
 
 	}
-	// ïåðåìåííûå äëÿ ïðîâåðêè ñòîëêíîâåíèé
+	// переменные для проверки столкновений
 	bool AllCollisions::CheckAllCollisions(sf::Sprite spriteOne, sf::Sprite spriteTwo)
 	{
 		sf::Rect<float> rectOne = spriteOne.getGlobalBounds();
 		sf::Rect<float> rectTwo = spriteTwo.getGlobalBounds();
-		// ïðîâåðêà ñòîëêíîâåíèÿ
+		// проверка столкновения
 		if (rectOne.intersects(rectTwo))
 		{
 			return true;
@@ -23,14 +23,14 @@ namespace SoundSonar
 		}
 	}
 
-	// áîëåå òî÷íûå ïåðåìåííûå äëÿ ïðîâåðêè ñòîëêíîâåíèé
+	// более точные переменные для проверки столкновений
 	bool AllCollisions::CheckAllCollisions(sf::Sprite spriteOne, float scaleOne, sf::Sprite spriteTwo, float scaleTwo)
 	{
 		spriteOne.setScale(scaleOne, scaleOne);
 		spriteTwo.setScale(scaleTwo, scaleTwo);
 		sf::Rect<float> rectOne = spriteOne.getGlobalBounds();
 		sf::Rect<float> rectTwo = spriteTwo.getGlobalBounds();
-		// ïðîâåðêà ñòîëêíîâåíèÿ
+		// проверка столкновения
 		if (rectOne.intersects(rectTwo))
 		{
 			return true;
